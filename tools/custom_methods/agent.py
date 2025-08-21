39fdd46adb44d888e47c498181573bf6eba24e19
Praveen K Singh <c2praveen30jun@gmail.com>
2025-08-21 11:15:14 +0530

    Update agent.py

--- tools/custom_methods/agent.py
+++ tools/custom_methods/agent.py
@@ -1,1 +1,32 @@
+from google.adk.agents import LlmAgent
+from dotenv import load_dotenv
+import requests
+
+# load environment variables 
+load_dotenv()
+
+def get_weather(city: str) -> str:
+    """Fetches the weather information for a given city in Celsius.
+    
+    Args:
+        city (str): Name of the city to get weather for
+        
+    Returns:
+        str: Weather information with temperature in Celsius
+    """
+    endpoint = "https://wttr.in"
+    # Adding m for metric units (Celsius)
+    response = requests.get(f"{endpoint}/{city}?m")
+    return response.text
+
+
+root_agent = LlmAgent(
+    name="first_agent",
+    description="This is my first agent",
+    instruction="You are a helpful assistant.",
+    model="gemini-2.0-flash",
+    tools = [get_weather]
+    
+)
+
 
